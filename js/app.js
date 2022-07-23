9fa9ed029ecc5b9bc5ef4c65437aafa1946d2775
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-23 15:36:02 -0600

    Adding proper comments

--- js/app.js
+++ js/app.js
@@ -1,42 +1,52 @@
-/* this function goes to the battle page if the user has chosen at least one pokemon */
+/* this function goes to the battle page if the user has chosen the pokemon */
 function goToBattle(details) {
-    if(Cookies.get(`selection`) !== undefined){
-        window.location.href = `selection.html`;
-    } else {
-        alert(`You should pick a Pokemon first.`);
-    }
+  /* this if checks if the json is not empty to then leave the user to the battle page */
+  if (Cookies.get(`selection`) !== undefined) {
+    window.location.href = `selection.html`;
+  } else {
+  /* else make alert in the page that tell the user to pick a pokemon */
+    alert(`You should pick a Pokemon first.`);
+  }
 }
 /* gettinh all a tags from the page */
 let go_to_battle = document.getElementsByClassName(`go-to-battle`);
 /* getting one a tag by one to on click call the function goToBattle */
-for(let counter = 0; counter < go_to_battle.length; counter ++){
-    go_to_battle[counter].addEventListener(`click`, goToBattle);
+for (let counter = 0; counter < go_to_battle.length; counter++) {
+  go_to_battle[counter].addEventListener(`click`, goToBattle);
 }
 
-/* This function creates the charizard object */
+/* this is the variable that creates the object Charizard */
 let charizard = {
-        name: `Charizard`,
-        attacks: [`Fire`, `Fly`, `Scratch`],
-        health: 150,
-        img_url: `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png`
-    };
+  name: `Charizard`,
+  attacks: [`Fire`, `Fly`, `Scratch`],
+  health: 150,
+  img_url: `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png`,
+};
 
+/* this function add the charizard to the battle */
 function addCharizard(details) {
-    /* making the object a JSON*/
-    let pokemon_selection_json = JSON.stringify(charizard);
-    /* setting the cookie with this JSON */
-    Cookies.set(`selection`, pokemon_selection_json);
+  /* making the object a JSON*/
+  let pokemon_selection_json = JSON.stringify(charizard);
+  /* setting the selection cookie with this JSON */
+  Cookies.set(`selection`, pokemon_selection_json);
 
-    let computer_pokemon_selection_json = JSON.stringify(venusaur);
-    Cookies.set(`computer_selection`, computer_pokemon_selection_json);
+  /* getting the computer pokemon and setting into a JSON */
+  let computer_pokemon_selection_json = JSON.stringify(venusaur);
+  /* setting the cookie of the computer_selection with the JSON */
+  Cookies.set(`computer_selection`, computer_pokemon_selection_json);
 
-    let user_current_health = charizard[`health`];
-    Cookies.set(`user_current_health`, user_current_health);
+  /* making the variable receive the pokemon user health number */
+  let user_current_health = charizard[`health`];
+  /* setting the cookie user_current_health with the JSON */
+  Cookies.set(`user_current_health`, user_current_health);
 
-    let computer_current_health = venusaur[`health`];
-    Cookies.set(`computer_current_health`, computer_current_health);
+  /* making the variable receive the pokemon computer health number */
+  let computer_current_health = venusaur[`health`];
+  /* setting the cookie computer_current_health with the JSON */
+  Cookies.set(`computer_current_health`, computer_current_health);
 
-    alert(`You picked ${charizard[`name`]}. Click in the Battle button now.`)
+  /* making an alert to tell the user which pokemon was picked and telling to click on the battle button */
+  alert(`You picked ${charizard[`name`]}. Click in the Battle button now.`);
 }
 
 /* getting the charizard button */
@@ -44,30 +54,38 @@ let add_charizard = document.getElementById(`button_charizard`);
 /* when the user clicks on this button, we call the function addCharizard */
 add_charizard.addEventListener(`click`, addCharizard);
 
-/* This function creates the venusaur object */
+/* this is the variable that creates the object Venusaur */
 let venusaur = {
-        name: `Venusaur`,
-        attacks: [`Leaf`, `Grass`, `Scratch`],
-        health: 150,
-        img_url: `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png`
-    };
+  name: `Venusaur`,
+  attacks: [`Leaf`, `Grass`, `Scratch`],
+  health: 150,
+  img_url: `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png`,
+};
 
+/* this function add the venusaur to the battle */
 function addVenusaur(details) {
-     /* making the object a JSON*/
-     let pokemon_selection_json = JSON.stringify(venusaur);
-     /* setting the cookie with this JSON */
-     Cookies.set(`selection`, pokemon_selection_json);
+  /* making the object a JSON*/
+  let pokemon_selection_json = JSON.stringify(venusaur);
+  /* setting the cookie with this JSON */
+  Cookies.set(`selection`, pokemon_selection_json);
 
-     let computer_pokemon_selection_json = JSON.stringify(blastoise);
-     Cookies.set(`computer_selection`, computer_pokemon_selection_json);
+  /* getting the computer pokemon and setting into a JSON */
+  let computer_pokemon_selection_json = JSON.stringify(blastoise);
+  /* setting the cookie of the computer_selection with the JSON */
+  Cookies.set(`computer_selection`, computer_pokemon_selection_json);
 
-     let user_current_health = venusaur[`health`];
-    Cookies.set(`user_current_health`, user_current_health);
+  /* making the variable receive the pokemon user health number */
+  let user_current_health = venusaur[`health`];
+  /* setting the cookie user_current_health with the JSON */
+  Cookies.set(`user_current_health`, user_current_health);
 
-    let computer_current_health = blastoise[`health`];
-    Cookies.set(`computer_current_health`, computer_current_health);
+  /* making the variable receive the pokemon computer health number */
+  let computer_current_health = blastoise[`health`];
+  /* setting the cookie computer_current_health with the JSON */
+  Cookies.set(`computer_current_health`, computer_current_health);
 
-    alert(`You picked ${venusaur[`name`]}. Click in the Battle button now.`)
+  /* making an alert to tell the user which pokemon was picked and telling to click on the battle button */
+  alert(`You picked ${venusaur[`name`]}. Click in the Battle button now.`);
 }
 
 /* getting the venusaur button */
@@ -75,30 +93,38 @@ let add_venusaur = document.getElementById(`button_venusaur`);
 /* when the user clicks on this button, we call the function addVenusaur */
 add_venusaur.addEventListener(`click`, addVenusaur);
 
-/* This function creates the blastoise object */
+/* this is the variable that creates the object Blastoise */
 let blastoise = {
-    name: `Blastoise`,
-    attacks: [`Water`, `Punch`, `Scratch`],
-    health: 150,
-    img_url: `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png`
-}
+  name: `Blastoise`,
+  attacks: [`Water`, `Punch`, `Scratch`],
+  health: 150,
+  img_url: `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png`,
+};
 
+/* this function add the blastoise to the battle */
 function addBlastoise(details) {
-    /* making the object a JSON*/
+  /* making the object a JSON*/
   let pokemon_selection_json = JSON.stringify(blastoise);
   /* setting the cookie with this JSON */
   Cookies.set(`selection`, pokemon_selection_json);
 
+  /* getting the computer pokemon and setting into a JSON */
   let computer_pokemon_selection_json = JSON.stringify(charizard);
+  /* setting the cookie of the computer_selection with the JSON */
   Cookies.set(`computer_selection`, computer_pokemon_selection_json);
 
+  /* making the variable receive the pokemon user health number */
   let user_current_health = blastoise[`health`];
-    Cookies.set(`user_current_health`, user_current_health);
+  /* setting the cookie user_current_health with the JSON */
+  Cookies.set(`user_current_health`, user_current_health);
 
-    let computer_current_health = charizard[`health`];
-    Cookies.set(`computer_current_health`, computer_current_health);
+  /* making the variable receive the pokemon computer health number */
+  let computer_current_health = charizard[`health`];
+  /* setting the cookie computer_current_health with the JSON */
+  Cookies.set(`computer_current_health`, computer_current_health);
 
-    alert(`You picked ${blastoise[`name`]}. Click in the Battle button now.`)
+  /* making an alert to tell the user which pokemon was picked and telling to click on the battle button */
+  alert(`You picked ${blastoise[`name`]}. Click in the Battle button now.`);
 }
 
 /* getting the blastoise button */
--- js/selection.js
+++ js/selection.js
@@ -1,28 +1,42 @@
+/* this function leave the user back to the pokemon options */
 function goBack(details) {
-    window.location.href = `index.html`
+  window.location.href = `index.html`;
 }
 
+/* this variable get the arrwo left from the page */
 let arrow_back = document.getElementById(`arrow-left`);
+/* by clicking in this arrow the function is called */
 arrow_back.addEventListener(`click`, goBack);
-arrow_back[`style`][`cursor`] = `pointer`;
 
 /* starting the selection as an empty string*/
 let selection = ``;
+
+/* starting the computer selection as an empty string*/
 let computer_selection = ``;
-/* receiveing the cookie value as json */
+
+/* receiveing the selection cookie value as json */
 let selection_json = Cookies.get(`selection`);
 
+/* receiveing the computer selection cookie value as json */
 let computer_selection_json = Cookies.get(`computer_selection`);
 
+/* receiveing the user current health as string */
 let user_current_health = parseInt(Cookies.get(`user_current_health`));
 
+/* receiveing the computer current health as string */
 let computer_current_health = parseInt(Cookies.get(`computer_current_health`));
 
 /* making sure that the selection is not undefined to set to an object */
-if(selection_json !== undefined) {
-    selection = JSON.parse(selection_json);
-    computer_selection = JSON.parse(computer_selection_json);
-    document.getElementById(`section-battle`).insertAdjacentHTML(`afterbegin`, 
+if (selection_json !== undefined) {
+  /* making the selection JSON back to an object again */
+  selection = JSON.parse(selection_json);
+
+  /* making the selection JSON back to an object again */
+  computer_selection = JSON.parse(computer_selection_json);
+
+  document.getElementById(`section-battle`).insertAdjacentHTML(
+    `afterbegin`,
+    /* adding to the page before the section the computer pokemon and then, the user pokemon */
     `<div>
         <img src="${computer_selection[`img_url`]}">
         <h2>${computer_selection[`name`]}</h2>
@@ -33,114 +47,172 @@ if(selection_json !== undefined) {
         <img class="img-battle" src="${selection[`img_url`]}">
         <h2 class="name-pokemon">${selection[`name`]}</h2>
         <div id="div-buttons-attack">
-        <button class="button-attacks" id="attack_one">${selection[`attacks`][0]}</button>
-        <button class="button-attacks" id="attack-two">${selection[`attacks`][1]}</button>
-        <button class="button-attacks" id="attack-three">${selection[`attacks`][2]}</button>
+        <button class="button-attacks" id="attack_one">${
+          selection[`attacks`][0]
+        }</button>
+        <button class="button-attacks" id="attack-two">${
+          selection[`attacks`][1]
+        }</button>
+        <button class="button-attacks" id="attack-three">${
+          selection[`attacks`][2]
+        }</button>
         </div>
         <h3 id="user-health">${user_current_health}hp</h3>
-    </div>`)
+    </div>`
+  );
 }
 
+/* this function its for the first button that can damage the computer pokemon */
 function damageFirst(details) {
-    computer_current_health = computer_current_health - 50;
+  /* the computer current health get decreased in a random number that can go from zero to 70 */
+  computer_current_health =
+    computer_current_health - Math.floor(Math.random() * 70);
 
-    if(computer_current_health <= 0) {
-        computer_current_health = 0;
-    }
+  /* check if the computer current health is less or equal to zero and if yes, set its value to zero */
+  if (computer_current_health <= 0) {
+    computer_current_health = 0;
+  }
 
-    let computer_health = document.getElementById(`computer-health`);
-    computer_health[`innerHTML`] = `${computer_current_health}hp`;
-    Cookies.set(`computer_current_health`, `${computer_current_health}`);
+  /* gets the computer health tag  */
+  let computer_health = document.getElementById(`computer-health`);
+  /* update the new value after being attacked */
+  computer_health[`innerHTML`] = `${computer_current_health}hp`;
+  /* set the cookie to this new value, so when the pages refreshs, the hp its still the same */
+  Cookies.set(`computer_current_health`, `${computer_current_health}`);
 
-    if(computer_current_health === 0) {
-        let computer_health = document.getElementById(`div-buttons-attack`);
+  /* check if the computer health after being attacked its equal to zero */
+  if (computer_current_health === 0) {
+    /* its print on the screen that you are the winner */
+    let computer_health = document.getElementById(`div-buttons-attack`);
     computer_health[`outerHTML`] = `<h3>You win!</h3>`;
-    } 
-    else if(user_current_health > 0) {
-        user_current_health = user_current_health - 40;
-        if(user_current_health <= 0){
-            user_current_health = 0;
-            let user_health = document.getElementById(`div-buttons-attack`);
-            user_health[`outerHTML`] = `<h3>You lost!</h3>`
-        }        
-        let user_health = document.getElementById(`user-health`);
-        user_health[`innerHTML`] = `${user_current_health}hp`;
-        Cookies.set(`user_current_health`, `${user_current_health}`);
-    } 
-    else {
-        
+  } else if (user_current_health > 0) {
+    /* check if the user current health is higher tham zero */
+    /* the user pokemon is attacked with a random number from zero to 50 */
+    user_current_health = user_current_health - Math.floor(Math.random() * 50);
+
+    /* check if the user current health is less or equal to zero */
+    if (user_current_health <= 0) {
+      /* set the user current health to zero */
+      user_current_health = 0;
+      /* print on the page that you lost */
+      let user_health = document.getElementById(`div-buttons-attack`);
+      user_health[`outerHTML`] = `<h3>You lost!</h3>`;
     }
+
+    /* gets the tag with the user health number */
+    let user_health = document.getElementById(`user-health`);
+    /* add the new value after being attacked */
+    user_health[`innerHTML`] = `${user_current_health}hp`;
+    /* uptading the cookie with the new user current health */
+    Cookies.set(`user_current_health`, `${user_current_health}`);
+  } else {
+  }
 }
 
+/* gets the id of the first attack button */
 let button_attack_one = document.getElementById(`attack_one`);
+/* after clicked, call the function damageFirst */
 button_attack_one.addEventListener(`click`, damageFirst);
 
+/* this function its for the second button that can damage the computer pokemon */
 function damageSecond(details) {
-    computer_current_health = computer_current_health - 40;
+  /* the computer current health get decreased in a random number that can go from zero to 60 */
+  computer_current_health =
+    computer_current_health - Math.floor(Math.random() * 60);
 
-    if(computer_current_health <= 0) {
-        computer_current_health = 0;
-    }
+  /* check if the computer current health is less or equal to zero and if yes, set its value to zero */
+  if (computer_current_health <= 0) {
+    computer_current_health = 0;
+  }
 
-    let computer_health = document.getElementById(`computer-health`);
-    computer_health[`innerHTML`] = `${computer_current_health}hp`;
-    Cookies.set(`computer_current_health`, `${computer_current_health}`);
+  /* gets the computer health tag  */
+  let computer_health = document.getElementById(`computer-health`);
+  /* update the new value after being attacked */
+  computer_health[`innerHTML`] = `${computer_current_health}hp`;
+  /* set the cookie to this new value, so when the pages refreshs, the hp its still the same */
+  Cookies.set(`computer_current_health`, `${computer_current_health}`);
 
-    if(computer_current_health === 0) {
-        let computer_health = document.getElementById(`div-buttons-attack`);
+  /* check if the computer health after being attacked its equal to zero */
+  if (computer_current_health === 0) {
+    /* its print on the screen that you are the winner */
+    let computer_health = document.getElementById(`div-buttons-attack`);
     computer_health[`outerHTML`] = `<h3>You win!</h3>`;
-    } 
-    else if(user_current_health > 0) {
-        user_current_health = user_current_health - 40;
-        if(user_current_health <= 0){
-            user_current_health = 0;
-            let user_health = document.getElementById(`div-buttons-attack`);
-            user_health[`outerHTML`] = `<h3>You lost!</h3>`
-        }        
-        let user_health = document.getElementById(`user-health`);
-        user_health[`innerHTML`] = `${user_current_health}hp`;
-        Cookies.set(`user_current_health`, `${user_current_health}`);
-    } 
-    else {
-        
+  } else if (user_current_health > 0) {
+    /* check if the user current health is higher tham zero */
+    /* the user pokemon is attacked with a random number from zero to 50 */
+    user_current_health = user_current_health - Math.floor(Math.random() * 50);
+
+    /* check if the user current health is less or equal to zero */
+    if (user_current_health <= 0) {
+      /* set the user current health to zero */
+      user_current_health = 0;
+      /* print on the page that you lost */
+      let user_health = document.getElementById(`div-buttons-attack`);
+      user_health[`outerHTML`] = `<h3>You lost!</h3>`;
     }
+
+    /* gets the tag with the user health number */
+    let user_health = document.getElementById(`user-health`);
+    /* add the new value after being attacked */
+    user_health[`innerHTML`] = `${user_current_health}hp`;
+    /* uptading the cookie with the new user current health */
+    Cookies.set(`user_current_health`, `${user_current_health}`);
+  } else {
+  }
 }
 
+/* gets the id of the second attack button */
 let button_attack_two = document.getElementById(`attack-two`);
-button_attack_two.addEventListener(`click`,
-damageSecond);
+/* after clicked, call the function damageSecond */
+button_attack_two.addEventListener(`click`, damageSecond);
 
+/* this function its for the third button that can damage the computer pokemon */
 function damageThird(details) {
-    computer_current_health = computer_current_health - 30;
+  /* the computer current health get decreased in a random number that can go from zero to 40 */
+  computer_current_health =
+    computer_current_health - Math.floor(Math.random() * 40);
 
-    if(computer_current_health <= 0) {
-        computer_current_health = 0;
-    }
+  /* check if the computer current health is less or equal to zero and if yes, set its value to zero */
+  if (computer_current_health <= 0) {
+    computer_current_health = 0;
+  }
 
-    let computer_health = document.getElementById(`computer-health`);
-    computer_health[`innerHTML`] = `${computer_current_health}hp`;
-    Cookies.set(`computer_current_health`, `${computer_current_health}`);
+  /* gets the computer health tag  */
+  let computer_health = document.getElementById(`computer-health`);
+  /* update the new value after being attacked */
+  computer_health[`innerHTML`] = `${computer_current_health}hp`;
+  /* set the cookie to this new value, so when the pages refreshs, the hp its still the same */
+  Cookies.set(`computer_current_health`, `${computer_current_health}`);
 
-    if(computer_current_health === 0) {
-        let computer_health = document.getElementById(`div-buttons-attack`);
+  /* check if the computer health after being attacked its equal to zero */
+  if (computer_current_health === 0) {
+    /* its print on the screen that you are the winner */
+    let computer_health = document.getElementById(`div-buttons-attack`);
     computer_health[`outerHTML`] = `<h3>You win!</h3>`;
-    } 
-    else if(user_current_health > 0) {
-        user_current_health = user_current_health - 40;
-        if(user_current_health <= 0){
-            user_current_health = 0;
-            let user_health = document.getElementById(`div-buttons-attack`);
-            user_health[`outerHTML`] = `<h3>You lost!</h3>`
-        }        
-        let user_health = document.getElementById(`user-health`);
-        user_health[`innerHTML`] = `${user_current_health}hp`;
-        Cookies.set(`user_current_health`, `${user_current_health}`);
-    } 
-    else {
-        
+  } else if (user_current_health > 0) {
+    /* check if the user current health is higher tham zero */
+    /* the user pokemon is attacked with a random number from zero to 50 */
+    user_current_health = user_current_health - Math.floor(Math.random() * 50);
+
+    /* check if the user current health is less or equal to zero */
+    if (user_current_health <= 0) {
+      /* set the user current health to zero */
+      user_current_health = 0;
+      /* print on the page that you lost */
+      let user_health = document.getElementById(`div-buttons-attack`);
+      user_health[`outerHTML`] = `<h3>You lost!</h3>`;
     }
+    /* gets the tag with the user health number */
+    let user_health = document.getElementById(`user-health`);
+    /* add the new value after being attacked */
+    user_health[`innerHTML`] = `${user_current_health}hp`;
+    /* uptading the cookie with the new user current health */
+    Cookies.set(`user_current_health`, `${user_current_health}`);
+  } else {
+  }
 }
 
-
+/* gets the id of the third attack button */
 let button_attack_third = document.getElementById(`attack-three`);
+/* after clicked, call the function damageThird */
 button_attack_third.addEventListener(`click`, damageThird);
