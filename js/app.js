36735ef19d61296f843a8b541a2dc8d389cdc889
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-22 18:49:05 -0600

    Addind proper comments

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
+/* starting the selection pokemon as an empty array */
 let pokemon_selection = [];
 
+/* This function creates the charizard object */
 function addCharizard(details) {
     let charizard = {
         name: `Charizard`,
@@ -8,14 +10,20 @@ function addCharizard(details) {
         img_url: `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/006.png`
     }
 
+    /* adding the object in the array */
     pokemon_selection.push(charizard);
+    /* making the array a JSON*/
     let pokemon_selection_json = JSON.stringify(pokemon_selection);
+    /* setting the cookie with this JSON */
     Cookies.set(`selection`, pokemon_selection_json);
 }
 
+/* getting the charizard button */
 let add_charizard = document.getElementById(`button_charizard`);
+/* when the user clicks on this button, we call the function addCharizard */
 add_charizard.addEventListener(`click`, addCharizard);
 
+/* This function creates the venusaur object */
 function addVenusaur(details) {
     let venusaur = {
         name: `Venusaur`,
@@ -24,14 +32,20 @@ function addVenusaur(details) {
         img_url: `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/003.png`
     }
 
+    /* adding the object in the array */
     pokemon_selection.push(venusaur);
+    /* making the array a JSON*/
     let pokemon_selection_json = JSON.stringify(pokemon_selection);
+    /* setting the cookie with this JSON */
     Cookies.set(`selection`, pokemon_selection_json);
 }
 
+/* getting the venusaur button */
 let add_venusaur = document.getElementById(`button_venusaur`);
+/* when the user clicks on this button, we call the function addVenusaur */
 add_venusaur.addEventListener(`click`, addVenusaur);
 
+/* This function creates the blastoise object */
 function addBlastoise(details) {
     let blastoise = {
         name: `Blastoise`,
@@ -39,11 +53,15 @@ function addBlastoise(details) {
         health: 150,
         img_url: `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/009.png`
     }
-
+    /* adding the object in the array */
     pokemon_selection.push(blastoise);
+    /* making the array a JSON*/
     let pokemon_selection_json = JSON.stringify(pokemon_selection);
+    /* setting the cookie with this JSON */
     Cookies.set(`selection`, pokemon_selection_json);
 }
 
+/* getting the blastoise button */
 let add_blastoise = document.getElementById(`button_blastoise`);
+/* when the user clicks on this button, we call the function addBlastoise */
 add_blastoise.addEventListener(`click`, addBlastoise);
--- js/selection.js
+++ js/selection.js
@@ -1,14 +1,21 @@
+
+/* starting the selection array as an empty array*/
 let selection_array = [];
+/* receiveing the cookie value as json */
 let selection_json = Cookies.get(`selection`);
 
+/* making sure that the selection is not undefined to set the selection_array to an array of objects */
 if(selection_json !== undefined) {
     selection_array = JSON.parse(selection_json);
 }
 
+/* for loop that walks the array of objects */
 for(let counter = 0; counter < selection_array.length; counter++) {
+    /* if the array is not undefined will add to the html information about the object selected */
     if(selection_array !== undefined) {
-        document.querySelector(`section`).insertAdjacentHTML(`afterend`, `
-        <div class="pokemon_card">
+        document.querySelector(`section`).insertAdjacentHTML(`afterend`,
+        /* insert in the screen the object with its image, name, attakcs and health */
+        `<div class="pokemon_card">
         <img class="img-pokemon" src="${selection_array[counter][`img_url`]}">
         <h2>${selection_array[counter][`name`]}</h2>
         <div id="buttons-attack">
@@ -22,28 +29,38 @@ for(let counter = 0; counter < selection_array.length; counter++) {
     }
 }
 
+/* get the section with its id */
 let section_battle = document.getElementById(`section-battle`);
+/* these two lines of code stylizes the elements being added to the page */
 section_battle[`style`][`display`] = `grid`;
 section_battle[`style`][`gridTemplateColumns`] = `repeat(auto-fit, minmax(250px, 1fr));`;
 
+/* getting all img of the page */
 let img_pokemon = document.getElementsByClassName(`img-pokemon`);
+/* giving all images a good size to fit well */
 for(let counter = 0; counter < img_pokemon.length; counter++) {
     img_pokemon[counter][`style`][`width`] = `300px`
 }
 
+/* getting all pokemons card that might appear in the page */
 let battle_section = document.getElementsByClassName(`pokemon_card`);
+/* giving all these cards some style to lok good */
 for(let counter = 0; counter < battle_section.length; counter++) {
     battle_section[counter][`style`][`display`] = `grid`;
     battle_section[counter][`style`][`placeItems`] = `center`;
     battle_section[counter][`style`][`rowGap`] = `15px`;
 }
 
+/* getting the div that contais buttons attack */
 let buttons_attack = document.getElementById(`buttons-attack`);
+/* styling the way that the buttons will display */
 buttons_attack[`style`][`display`] = `grid`;
 buttons_attack[`style`][`gridTemplateColumns`] = `1fr 1fr 1fr`;
 buttons_attack[`style`][`columnGap`] = `15px`;
 
+/* getting all buttons of the page */
 let buttons = document.querySelectorAll(`button`);
+/* styling all buttons to look good */
 for(let counter = 0; counter < buttons.length; counter++) {
     buttons[counter][`style`][`width`] = `60px`;
     buttons[counter][`style`][`height`] = `25px`;
